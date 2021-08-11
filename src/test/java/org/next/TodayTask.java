983ed19ee8c4fd4aa0aa7019cd045fe68257f846
kabasaba <kavinsaba14@gmail.com>
2021-08-11 10:10:22 +0530

    target push

---
+++ src/test/java/org/next/TodayTask.java
@@ -0,0 +1,18 @@
+package org.next;
+
+public class TodayTask {
+	
+	public int add(int a , int b) {
+		
+		int c= a+b;
+		return c ;
+		
+	}	
+	
+	public int add1(int b , int g) {
+		int a= b+g;
+		return a;
+
+	}
+	
+}
